4d3cc8ea1fb28b7901c2f088ddc66ec2d9d64beb
Marco <marcogaruthi@hotmail.com>
2020-05-09 13:29:39 -0300

    corrige testes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-test("renders learn react link", () => {
+test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(
+    /Discovery all the numbers related to the corona virus in your country! /i
+  );
   expect(linkElement).toBeInTheDocument();
 });
